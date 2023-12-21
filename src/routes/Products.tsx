3974664ce7577092c70e354c21a8cba52378dc66
DavidxDl <Davidbekamlopez@gmail.com>
2023-12-21 04:28:38 -0300

    added some products to test i need to fix some bugs with the style

--- src/routes/Products.tsx
+++ src/routes/Products.tsx
@@ -1,14 +1,33 @@
+import { useEffect, useState } from "react";
 import Header from "../components/Header/Header";
 import NavBar from "../components/NavBar/NavBar";
 
 export default function Products() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function getProducts() {
+      const response = await fetch("https://fakestoreapi.com/products")
+      const data = await response.json();
+      console.log(data);
+      setProducts(data)
+    }
+    getProducts();
+  }, [])
   return (
     <>
       <Header />
       <NavBar />
       <main>
-        hi
-      </main>
+        {products.map(product => (
+          <div key={product.id} style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyItems: "center", margin: "10px" }}>
+            <img style={{ width: "150px" }} src={product.image} />
+            <h2>{product.title}</h2>
+            <p>{product.description}</p>
+            <button>Add to cart</button>
+          </div>
+        ))}
+      </main >
     </>
   )
 }
